bd522d980426414dd355ff3f4377d0472ad16c98
steve.wojcik <steve.wojcik@mrm.com>
2020-05-15 18:29:20 -0400

    Source

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  // executes when HTML-Document is loaded and DOM is ready
+  console.log("document is ready");
+  
+  $( ".header-card" ).hover(
+    function() {
+      $(this).addClass('purple-card').css('cursor', 'pointer'); 
+      $(".card-text", this).addClass('white');
+      $(".card-text", this).addClass('alpha');
+      $(".icon-dark", this).addClass('hide-me');
+      $(".icon-light", this).addClass('show-me');
+      // $(this).find(".header-card .card-text").addClass('light'); 
+    }, function() {
+      $(this).removeClass('purple-card');
+      $(".card-text", this).removeClass('white');
+      $(".card-text", this).removeClass('alpha');
+      $(".icon-dark", this).removeClass('hide-me');
+      $(".icon-light", this).removeClass('show-me');
+    }
+  );
+
+  $( ".dark-card" ).hover(
+    function() {
+      $(this).addClass('white-card').css('cursor', 'pointer'); 
+      $(".card-text", this).addClass('dark');
+      $(".icon-purple", this).addClass('show-me');
+      $(".icon-white", this).addClass('hide-me');
+      $(".card-bullet", this).addClass('medium');
+      // $(this).find(".header-card .card-text").addClass('light'); 
+    }, function() {
+      $(this).removeClass('white-card');
+      $(".card-text", this).removeClass('dark');
+      $(".icon-purple", this).removeClass('show-me');
+      $(".icon-white", this).removeClass('hide-me');
+      $(".card-bullet", this).removeClass('medium');
+    }
+  );
+  
+// document ready  
+});
+
