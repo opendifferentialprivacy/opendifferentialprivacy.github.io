815a32641353fe8d4ef53c8251331ec394a19942
steve.wojcik <steve.wojcik@mrm.com>
2020-05-18 17:22:43 -0400

    no message

--- script.js
+++ script.js
@@ -35,6 +35,14 @@ $(document).ready(function() {
       $(".card-bullet", this).removeClass('medium');
     }
   );
+
+     $(document).click(function (event) {
+         var clickover = $(event.target);
+         var _opened = $(".navbar-collapse").hasClass("show");
+         if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+             $(".navbar-toggler").click();
+         }
+     });
   
 // document ready  
 });
